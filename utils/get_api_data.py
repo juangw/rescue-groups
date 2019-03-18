29b18fd8934515d5954b2ccf3f0db4e100f058b8
juangw <juangw@umich.edu>
2019-03-18 10:32:29 -0400

    add more fields to required

--- utils/get_api_data.py
+++ utils/get_api_data.py
@@ -9,7 +9,7 @@ logger = logging.getLogger()
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
-REQUEST = namedtuple("request_data", ["url", "api_key"])
+REQUEST = namedtuple("request_data", ["url", "data", "api_key"])
 
 
 def get_api_key(topic):
@@ -17,18 +17,20 @@ def get_api_key(topic):
     with open("../secrets.json") as secrets:
         topic_secret = json.load(secrets).get(topic)
         topic_url = topic_secret.get("url")
-        topic_api_key = topic_secret.get("api_key")
-        return REQUEST(topic_url, topic_api_key)
+        topic_data = topic_secret.get("data")
+        topic_api_key = topic_secret.get("api_key", "")
+        return REQUEST(topic_url, topic_data, topic_api_key)
 
 
 def call_api_url(topic):
     """Sends get request to the given api url"""
-    api_data = get_api_key(topic)
-    api_key = api_data.api_key
-    api_url = api_data.url
+    api_info = get_api_key(topic)
+    api_url = api_info.url
+    api_data = api_info.data
+    api_key = api_info.api_key
     headers = {"x-api-key": api_key}
-    response = requests.get(
-            url=api_url, headers=headers, timeout=5
+    response = requests.post(
+            url=api_url, headers=headers, json=api_data, timeout=5
     )
     print(response.text)
 
