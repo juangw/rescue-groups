b65bd13d202977321e3ddb8db53f8ae8e4740de2
juangw <juangw@umich.edu>
2019-03-26 00:11:42 -0400

    hook up to connexion

--- api_projects/server.py
+++ api_projects/server.py
@@ -1,9 +1,7 @@
 from common.utils.get_api_data import api_post_req
-from flask import (
-    Flask,
-    render_template
-)
+from flask import render_template
 
+import connexion
 import logging
 
 logger = logging.getLogger()
@@ -13,7 +11,7 @@ logger.setLevel(logging.DEBUG)
 logger.debug("Start of Program, Testing Debugger")
 
 # Create the application instance
-app = Flask(__name__)
+app = connexion.FlaskApp(__name__, specification_dir='openapi/')
 
 
 @app.route("/")
@@ -24,39 +22,44 @@ def home():
 
     :return:        the rendered template "home.html"
     """
-    # default_filter = [
-    #     {
-    #         "fieldName": "animalSpecies",
-    #         "operation": "equals",
-    #         "criteria": "cat"
-    #     },
-    #     {
-    #         "fieldName": "animalLocation",
-    #         "operation": "equals",
-    #         "criteria": "48105"
-    #     }
-    # ]
-    # default_fields = [
-    #     "animalID",
-    #     "animalOrgID",
-    #     "locationPhone",
-    #     "animalName",
-    #     "animalThumbnailUrl",
-    #     "animalPictures",
-    #     "animalSex",
-    #     "animalAdoptionFee",
-    #     "animalBreed",
-    #     "animalColor",
-    #     "animalEyeColor",
-    #     "animalAgeString",
-    #     "animalLocation",
-    #     "animalAffectionate",
-    #     "animalApartment",
-    #     "animalIntelligent",
-    #     "animalLap",
-    #     "animalActivityLevel"
-    # ]
+    default_filter = [
+        {
+            "fieldName": "animalSpecies",
+            "operation": "equals",
+            "criteria": "cat"
+        },
+        {
+            "fieldName": "animalLocation",
+            "operation": "equals",
+            "criteria": "48105"
+        }
+    ]
+    default_fields = [
+        "animalID",
+        "animalOrgID",
+        "locationPhone",
+        "animalName",
+        "animalThumbnailUrl",
+        "animalPictures",
+        "animalSex",
+        "animalAdoptionFee",
+        "animalBreed",
+        "animalColor",
+        "animalEyeColor",
+        "animalAgeString",
+        "animalLocation",
+        "animalAffectionate",
+        "animalApartment",
+        "animalIntelligent",
+        "animalLap",
+        "animalActivityLevel"
+    ]
 
-    # text = api_post_req("rescue_group", default_filter, default_fields)
-    # print(text)
+    text = api_post_req("rescue_group", default_filter, default_fields)
+    print(text)
     return render_template("home.html")
+
+
+def run():
+    app.add_api('my_api.yaml')
+    app.run(debug=True, port=8080)
--- common/utils/get_api_data.py
+++ common/utils/get_api_data.py
@@ -1,3 +1,4 @@
+from common.utils.get_secrets import get_secret
 from collections import namedtuple
 
 import requests
@@ -13,12 +14,12 @@ REQUEST = namedtuple("request_data", ["url", "data", "api_key"])
 
 def get_api_key(topic):
     """Gets api key based on url"""
-    with open("../../secrets.json") as secrets:
-        topic_secret = json.load(secrets).get(topic)
-        topic_url = topic_secret.get("url")
-        topic_data = topic_secret.get("data")
-        topic_api_key = topic_secret.get("api_key", "")
-        return REQUEST(topic_url, topic_data, topic_api_key)
+    secrets = get_secret().decode()
+    topic_secret = json.loads(secrets).get(topic)
+    topic_url = topic_secret.get("url")
+    topic_data = topic_secret.get("data")
+    topic_api_key = topic_secret.get("api_key", "")
+    return REQUEST(topic_url, topic_data, topic_api_key)
 
 
 def api_post_req(topic, filters=[], fields=[]):
