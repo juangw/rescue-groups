1a606eda4e297decaabf6621a3a21e5efd8d0ee8
juangw <juangw@umich.edu>
2019-05-18 10:59:00 -0400

    add config vars

--- common/utils/construct_payload.py
+++ common/utils/construct_payload.py
@@ -2,15 +2,33 @@ from common.utils.get_secrets import get_secret
 from collections import namedtuple
 
 import json
+import os
 
+STAGE = os.environ.get("STAGE", "local")
 REQUEST = namedtuple("request_data", ["url", "data", "api_key"])
 
 
 def get_api_key(topic):
     """Gets api key based on url"""
-    secrets = get_secret().decode()
-    topic_secret = json.loads(secrets).get(topic)
-    topic_url = topic_secret.get("url")
-    topic_data = topic_secret.get("data")
-    topic_api_key = topic_secret.get("api_key", "")
+    if STAGE is "local":
+        secrets = get_secret().decode()
+        topic_secret = json.loads(secrets).get(topic)
+        topic_url = topic_secret.get("url")
+        topic_data = topic_secret.get("data")
+        topic_api_key = topic_secret.get("api_key", "")
+    else:
+        topic_url = os.environ.get("url")
+        topic_api_key = os.environ.get("api_key", "")
+        topic_data = {
+            "apikey": topic_api_key,
+            "objectType": "animals",
+            "objectAction": "publicSearch",
+            "search": {
+                "resultSort": "animalID",
+                "resultOrder": "asc",
+                "calcFoundRows": "Yes",
+                "filters": [],
+                "fields": []
+            }
+        }
     return REQUEST(topic_url, topic_data, topic_api_key)
