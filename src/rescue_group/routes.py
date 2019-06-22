e5876dd48fd950d0dba08ff55177dd4ceaad1e33
juangw <juangw@umich.edu>
2019-06-22 10:25:04 -0400

    use javascript history back

--- src/rescue_group/routes.py
+++ src/rescue_group/routes.py
@@ -97,7 +97,6 @@ def animals_saved():
 
 
 @app.route("/animals/<page>", methods=["GET", "POST"])
-@app.route("/animals/distance-<distance>", methods=["GET", "POST"])
 @app.route("/animals/<page>/distance-<distance>", methods=["GET", "POST"])
 @app.route("/animals/<page>/age-<age>/distance-<distance>", methods=["GET", "POST"])
 @app.route("/animals/<page>/gender-<gender>/distance-<distance>", methods=["GET", "POST"])
